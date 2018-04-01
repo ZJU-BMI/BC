cd5788fd33224873dc59d5b65437b0566a86055a
gzx-ubuntu <1013168797@qq.com>
2018-04-01 14:37:49 +0800

    init commit

---
+++ config/webpack.config.js
@@ -0,0 +1,91 @@
+// import webpack from 'webpack';
+// import path from 'path';
+// import merge from 'webpack-merge'
+const webpack = require('webpack');
+const path = require('path')
+const merge = require('webpack-merge')
+
+const ROOT = path.join(__dirname, "..");
+const CLIENT = path.join(ROOT, "client");
+const TARGET = process.env.npm_lifecycle_event
+
+const common = {
+    entry: {
+        app: CLIENT,
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(ROOT, 'public'),
+        publicPath: '/assets/static'
+    },
+    // resolve: {
+    //     extention: ['.js', '.jsx'],
+    //     modules: [
+    //         'node_modules',
+    //     ]
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: CLIENT,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                    'style-loader'
+                ]
+            },
+            {
+                test: /\.json$/,
+                use: 'json-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name(file) {
+                                if (env === 'development') {
+                                    return '[path][name].[ext]';
+                                }
+                                return '[hash].[ext]';
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+const dev = {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBasse: './dist'
+    }
+}
+
+const prod = {
+    mode: "production",
+    devtool: 'source-map',
+    // plugins: [
+    //     new webpack.DefinePlugin({
+    //         'process.env.NODE_ENV': JSON.stringify('production')
+    //     }),
+    //     new webpack.optimize.UglifyJsPlugin()
+    // ]
+}
+
+if (TARGET === "start" ||
+    TARGET === "test") {
+    module.exports = merge(common, dev)
+} else if (TARGET === "build:client") {
+    module.exports = merge(common, prod)
+} else {
+    throw new Error(`target ${TARGET} was not specified, please checkout webpack.config.js`)
+}
---
+++ server/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+import webpack from "webpack";
+import config from "../config/webpack.config.js";
+
+const compiler = webpack(config);
+
+const port = 3000;
+const app = express();
+
+app.listen(port, function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        open(`http://localhost:${port}`);
+    }
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'assets' , 'index.html'));
+});
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
